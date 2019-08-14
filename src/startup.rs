461d3b55f8760e69e86bb2f5a29c799e82b66edb
Branan Riley <branan@gmail.com>
2019-08-13 17:32:27 -0700

    Initialize BSS at the start of DTCM
    This adds BSS initialization, and also moves the BSS section from RAM
    to DTCM as was done for the boot stack, since I'm still unsure why the
    OCRAM doesn't want to work.

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,24 @@
 extern "C" {
     fn main();
+    static _bss_start: u8;
+    static _bss_end: u8;
 }
 
 #[link_section = ".startup"]
 #[no_mangle]
 pub extern "C" fn startup() {
+    init_bss();
     unsafe { main() }
+}
+
+#[link_section = ".startup"]
+fn init_bss() {
+    unsafe {
+        let mut ptr = &_bss_start as *const u8 as *mut u8;
+        let end = &_bss_end as *const u8 as  *mut u8;
+        while ptr != end {
+            core::ptr::write_volatile(ptr, 0);
+            ptr = (ptr as usize +1) as *mut u8;
+        }
+    }
 }
