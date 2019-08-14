f43697514197dda272d948dcf74e8491c9f5af4d
Branan Riley <branan@gmail.com>
2019-08-13 17:47:33 -0700

    Fix BSS initialization when it is a zero-length segment

--- src/startup.rs
+++ src/startup.rs
@@ -14,9 +14,16 @@ pub extern "C" fn startup() {
 #[link_section = ".startup"]
 fn init_bss() {
     unsafe {
+        // This is probably fragile.
+        // As far as I can tell, the optimizer is assuming that
+        // _bss_start and _bss_end cannot be aliases for the same
+        // memory location. This causes a zero-length BSS section to
+        // blow up here. re-creating our end pointer with a bit of
+        // math seems to trick the optimizer, for now.
+        let length = (&_bss_end as *const u8 as usize) - (&_bss_start as *const u8 as usize);
         let mut ptr = &_bss_start as *const u8 as *mut u8;
-        let end = &_bss_end as *const u8 as *mut u8;
-        while ptr != end {
+        let end = (ptr as usize + length) as *const u8;
+        while ptr as *const u8 != end {
             core::ptr::write_volatile(ptr, 0);
             ptr = (ptr as usize + 1) as *mut u8;
         }
